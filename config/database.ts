907527e352af1691f5fe3077f24cc82687bb421e
Romain Calmelet <romaincalmelet@gmail.com>
2025-12-22 14:51:07 +0100

    chore: initial base commit – Next.js + Strapi template

---
+++ config/database.ts
@@ -0,0 +1,29 @@
+export default ({ env }) => {
+  const client = env('DATABASE_CLIENT', 'sqlite')
+
+  if (client === 'sqlite') {
+    return {
+      connection: {
+        client: 'sqlite',
+        connection: {
+          filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        },
+        useNullAsDefault: true,
+      },
+    }
+  }
+
+  return {
+    connection: {
+      client: 'postgres',
+      connection: {
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        user: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
+        ssl: env.bool('DATABASE_SSL', false),
+      },
+    },
+  }
+}
